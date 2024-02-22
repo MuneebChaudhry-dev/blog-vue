a1e5557735f1fd61ad14133c104c130df9258fa6
Muneeb <muneebchaudhry.dev@gmail.com>
2024-02-22 15:23:24 +0500

    Add JsonServer and Integrated by making composable

---
+++ src/components/BlogPosts.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <pre>{{ data }}</pre>
+  </div>
+</template>
+<script setup lang="ts">
+import { ref, watch } from 'vue'
+import { useFetch } from '../composable/fetch'
+const { data, error } = useFetch('http://localhost:3000/posts')
+watch(data, (newData) => {
+  console.log(`x is ${newData}`)
+})
+</script>
+<style></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import NavbarVue from '@/components/NavbarVue.vue'
 import BlogForm from '@/components/BlogForm.vue'
+import BlogPosts from '@/components/BlogPosts.vue'
 </script>
 
 <template>
   <NavbarVue />
   <BlogForm />
+  <BlogPosts />
 </template>
