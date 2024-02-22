a3b8b51173018385cdae7e9857ee1ee2b6a3cdf6
Muneeb <muneebchaudhry.dev@gmail.com>
2024-02-22 12:19:56 +0500

    Basic Setup

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div class="container">
+    <header>
+      <div>
+        <nav>
+          <RouterLink to="/">Home</RouterLink>
+        </nav>
+      </div>
+    </header>
+
+    <RouterView />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main><h1 class="bg-black text-white">This is Vue</h1></main>
+</template>
