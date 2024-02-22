7b0874673d922c2adef568e8092f5a88d3d58f21
Muneeb <muneebchaudhry.dev@gmail.com>
2024-02-22 14:42:32 +0500

    Add Post Form

---
+++ src/components/BlogForm.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex justify-center flex-col items-center gap-2">
+    <div class="form-input">
+      <input type="text" placeholder="Enter Your Post Title" />
+    </div>
+    <div class="form-input">
+      <textarea type="text" placeholder="Enter Your Post Description"></textarea>
+    </div>
+    <div class="w-96 flex justify-end">
+      <button>Post</button>
+    </div>
+  </div>
+</template>
+<script lang="ts" setup></script>
+<style></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import NavbarVue from '@/components/NavbarVue.vue'
+import BlogForm from '@/components/BlogForm.vue'
 </script>
 
 <template>
   <NavbarVue />
+  <BlogForm />
 </template>
